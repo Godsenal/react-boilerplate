47a55dfe31d86a1364325fe3e4e5c4d9fcf292df
Godsenal <tmqps78@gmail.com>
2018-07-25 21:12:11 +0900

    Add reselect

--- src/containers/TodoPage/index.js
+++ src/containers/TodoPage/index.js
@@ -4,11 +4,32 @@ import { connect } from 'react-redux';
 import { FilterTab, TodoInput, TodoItem } from '../../components';
 import * as TodoActions from '../../actions/todo';
 import * as FilterActions from '../../actions/filter';
+import { getFilteredTodos } from '../../reducers/todo';
 import styles from './TodoPage.scss';
 
 const FILTER_TYPES = ['ALL', 'INCOMPLETED', 'COMPLETED'];
+
+const TodoPage = ({ filteredTodos, filter, addTodo, toggleTodo, setFilter }) => (
+  <div className={styles.container}>
+    <TodoInput addTodo={addTodo} />
+    <FilterTab filter={filter} filterTypes={FILTER_TYPES} setFilter={setFilter} />
+    {
+      filteredTodos.map(todo => <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />)
+    }
+  </div>
+);
+
+TodoPage.propTypes = {
+  addTodo: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+  filteredTodos: PropTypes.array.isRequired,
+  setFilter: PropTypes.func.isRequired,
+  toggleTodo: PropTypes.func.isRequired,
+};
+
+
 const mapStateToProps = state => ({
-  todos: state.todo.todos,
+  filteredTodos: getFilteredTodos(state),
   filter: state.filter.filter,
 });
 const mapDispatchToProps = dispatch => ({
@@ -16,37 +37,5 @@ const mapDispatchToProps = dispatch => ({
   toggleTodo: (id) => dispatch(TodoActions.toggleTodo(id)),
   setFilter: (filter) => dispatch(FilterActions.setFilter(filter)),
 });
-
-const TodoPage = ({ todos, filter, addTodo, toggleTodo, setFilter }) => {
-  const getVisibleTodo = list => {
-    switch (filter) {
-      case 'ALL':
-        return list;
-      case 'COMPLETED':
-        return list.filter(todo => todo.done);
-      case 'INCOMPLETED':
-        return list.filter(todo => !todo.done);
-      default:
-        return list;
-    }
-  };
-  return (
-    <div className={styles.container}>
-      <TodoInput addTodo={addTodo} />
-      <FilterTab filter={filter} filterTypes={FILTER_TYPES} setFilter={setFilter} />
-      {
-        getVisibleTodo(todos).map(todo => <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />)
-      }
-    </div>
-  );
-};
-
-TodoPage.propTypes = {
-  addTodo: PropTypes.func.isRequired,
-  filter: PropTypes.string.isRequired,
-  setFilter: PropTypes.func.isRequired,
-  todos: PropTypes.array.isRequired,
-  toggleTodo: PropTypes.func.isRequired,
-};
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoPage);
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,5 +1,27 @@
+import { createSelector } from 'reselect';
 import { ADD_TODO, TOGGLE_TODO } from '../constants/actionTypes';
 
+
+/* Selector */
+const getTodos = state => state.todo.todos;
+const getFilter = state => state.filter.filter;
+
+export const getFilteredTodos = createSelector(
+  [getTodos, getFilter],
+  (todos, filter) => {
+    switch (filter) {
+      case 'ALL':
+        return todos;
+      case 'INCOMPLETED':
+        return todos.filter(item => !item.done);
+      case 'COMPLETED':
+        return todos.filter(item => item.done);
+      default:
+        return todos;
+    }
+  }
+);
+/* Reducer */
 const initialState = {
   todos: [],
 };
